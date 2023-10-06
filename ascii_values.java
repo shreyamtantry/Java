5fae74541c755f0f1610b9f80de80aac2b9d193d
shreyamtantry <145944974+shreyamtantry@users.noreply.github.com>
2023-10-06 20:00:21 +0530

    Add files via upload

---
+++ ascii_values.java
@@ -0,0 +1,26 @@
+package com.sangamone;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ascii_values {
+
+	
+
+	
+	    public static void main(String[]args)
+	    {
+	        for(char c='A';c<='Z';c++)
+	            System.out.print(c);
+	        System.out.println();
+	        int num1=65;
+	        int num2=97;
+	        for(int i=0;i<26;i++)
+	        {
+	        	System.out.print((char)num1+i);
+	        	
+	        }
+	    }
+}
+
+
+	    
